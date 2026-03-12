0d508ce380ffbc99d7e06a25ba1acc6743d49e27
sagynov <kzxit99@gmail.com>
2026-03-12 15:10:34 +0500

    user factory deleted

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::firstOrCreate([
           'email' => 'admin@bot.poscenter.kz',
         ], [
